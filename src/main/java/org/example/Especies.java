8f52b5b7b3faa267daadbb59440960d2e401bb69
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2024-03-06 10:59:38 +0100

    Creación clase especies

---
+++ src/main/java/org/example/Especies.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Especies {
+    public String especie;
+
+    public Especies() {
+        especie = "No definido";
+    }
+    public Especies(String especie) {
+        this.especie = especie;
+    }
+    public String getEspecie() {
+        return especie;
+    }
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    @Override
+    public String toString() {
+        return "Especie: " + especie;
+    }
+}
